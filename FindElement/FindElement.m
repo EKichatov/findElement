9f744876efa40575d927582e5258cb4666044cc5
Developer <developer@Mac-mini-Developer.local>
2016-07-29 18:50:49 +0300

    addAlgorithm

--- FindElement/FindElement.m
+++ FindElement/FindElement.m
@@ -10,4 +10,14 @@
 
 @implementation FindElement
 
++ (NSInteger)findElement:(NSArray<NSNumber *> *)array number:(NSInteger)number
+{
+    for(NSInteger i=array.count-1;i>=0;i--)
+    {
+        if(array[i].integerValue<=number)
+            return i;
+    }
+    return -1;
+}
+
 @end
