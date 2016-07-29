25f7aad51a8ac65ab8611c0a4bcbf63b89753b3c
Developer <developer@Mac-mini-Developer.local>
2016-07-29 18:34:23 +0300

    add class

---
+++ FindElement/FindElement.h
@@ -0,0 +1,13 @@
+//
+//  FindElement.h
+//  FindElement
+//
+//  Created by Developer on 29/07/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FindElement : NSObject
+
+@end
---
+++ FindElement/FindElement.m
@@ -0,0 +1,13 @@
+//
+//  FindElement.m
+//  FindElement
+//
+//  Created by Developer on 29/07/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+#import "FindElement.h"
+
+@implementation FindElement
+
+@end
